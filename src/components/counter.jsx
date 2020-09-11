3c536d4f3f6ee879de68ae1ba1e29b9d7d8fdbf7
Romain BOBOE <rboboe@gmail.com>
2020-09-11 18:02:07 -0400

    adding second component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,40 +2,39 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        value: this.props.value,
     };
-    
+
     handleIncrement = () => {
-        console.log('Increment Clicked', this); 
-    }
+        this.setState({ value: this.state.value + 1 }); 
+    };
 
     render() { 
+        console.log(this.props);
         return (
             <React.Fragment>
+
                 <span style={{ fontSize: 18, }} className={this.getBadgeClasses()}>{this.formatCount()} </span>
                 <button 
                     onClick={this.handleIncrement}
                     className="btn btn-secondary btn-sm"
                     >
-                        Increment
-                </button>
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
-            </React.Fragment>            
+                        Increment 
+                </button> 
+                <button className="btn btn-danger btn-sm m-2">Delete</button>
+            </React.Fragment>             
             );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const { count } = this.state;
-        return count === 0 ? "Zero" : count;
+        const { value } = this.state;
+        return value === 0 ? "Zero" : value;
     }
 }
  
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import Counters from './components/counters';
 ReactDOM.render(
-  <Counter/>,
+  <Counters/>,
   document.getElementById('root')
 );
 
